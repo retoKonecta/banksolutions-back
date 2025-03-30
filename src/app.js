0da24ad3ebf78721966673256210fc85dafa5bc9
aqcarolina <ariasquicenocarolina@gmail.com>
2025-03-30 13:57:15 -0500

    Conexión BD y creación de token

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
-import morgan from 'morgan';
+import morgan from "morgan";
+
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express()
 
 app.use(morgan('dev'));
+app.use(express.json());
 
-export default app;
+app.use("/api", authRoutes);
+
+export default app;   
